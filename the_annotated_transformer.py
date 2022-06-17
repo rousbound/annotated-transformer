124b2d1bb42cbc949276f5a2e1f59431fedc7e10
Geraldo Luiz <geraldoluiz@puc-rio.br>
2022-06-17 10:25:40 -0300

    Push

--- the_annotated_transformer.py
+++ the_annotated_transformer.py
@@ -1163,11 +1163,13 @@ def check_outputs(
         rb = Batch(b[0], b[1], pad_idx)
         greedy_decode(model, rb.src, rb.src_mask, 64, 0)[0]
 
+        print("RB SRC:", rb.src)
+        print("RB SRC type:", type(rb.src))
         src_tokens = [
-            vocab_src.get_itos()[x] for x in rb.src if x != pad_idx
+            vocab_src.get_itos()[x] for x in rb.src[0] if x != pad_idx
         ]
         tgt_tokens = [
-            vocab_tgt.get_itos()[x] for x in rb.tgt if x != pad_idx
+            vocab_tgt.get_itos()[x] for x in rb.tgt[0] if x != pad_idx
         ]
 
         print(
